490f1f34d91fcd8a724322716b199f63d701116d
csra-reddy <chandrasekharreddyanam.sitm@gmail.com>
2023-08-30 10:34:26 +0530

    Initial commit

---
+++ mailing-consumer/src/main/java/org/example/KafkaTopicListener.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaTopicListener {
+
+    @KafkaListener(topics = {"packaging_mailing_topic"}, groupId = "mail-group")
+    public void handleSendingEmail(Mail mail) {
+        System.out.println("Preparing to send shipping info to this email [" + mail.getEmail() + "]");
+        System.out.println("Shipping Information");
+        System.out.println(mail);
+    }
+}
---
+++ src/main/java/com/docker/kafka/docker_kafka/DockerKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.docker.kafka.docker_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerKafkaApplication.class, args);
+    }
+
+}
